01fc441e5261538bfb381de332fe2140fef62aea
Shiro <115549126+RifqiAryaS@users.noreply.github.com>
2023-07-22 19:24:36 +0700

    Update knn.py

--- Tugas Akhir/knn.py
+++ Tugas Akhir/knn.py
@@ -41,23 +41,6 @@ acc = accuracy_score(y_test, y_pred)
 print(cm)
 print(acc)
 
-# membuat diagram untuk data train
-# x_set, y_set = x_train, y_train
-# x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
-#                      np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01))
-# plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(
-#     x1.shape), alpha=0.75, cmap=ListedColormap(('red', 'green')))
-# plt.xlim(x1.min(), x1.max())
-# plt.ylim(x2.min(), x2.max())
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-#                 c=ListedColormap(('red', 'green'))(i), label=j)
-# plt.title('Klasifikasi Data untuk Data Training dengan K-NN')
-# plt.xlabel('Glukosa')
-# plt.ylabel('Tekanan Darah')
-# plt.legend()
-# plt.show()
-
 # membuat diagram untuk data test
 x_set, y_set = x_test, y_test
 x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
